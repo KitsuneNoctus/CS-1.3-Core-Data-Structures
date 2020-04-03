f86762e19dcb03e9b3b0a049f6fa4080527ff1ea
Henry Calderon <theceintiant@gmail.com>
2020-04-02 19:18:40 -0700

    Finished assignment.

--- Code/number-bases/bases.py
+++ Code/number-bases/bases.py
@@ -74,18 +74,8 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    if base1 == '2' and base2 == '16':
-        print("Converting base 2 to base 16")
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    elif base1 == '2' and base2 == '10':
-        print("Converting base 2 to base 10")
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    elif base1 == '10' and base2 == '16':
-        print("Converting base 10 to base 16")
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    else:
-        print("Converting any bases")
+
+    return encode(decode(digits, base1),base2)
 
 
 def main():

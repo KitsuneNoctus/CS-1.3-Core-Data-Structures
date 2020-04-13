7d4f8085757e33521eafee2fe81a136e8f19798a
Henry Calderon <theceintiant@gmail.com>
2020-04-12 17:54:28 -0700

    Working on strings. Difficulties faced.

--- Code/palindromes-and-strings/strings.py
+++ Code/palindromes-and-strings/strings.py
@@ -23,6 +23,27 @@ def contains(text, pattern):
 
     # if pattern == '':
     #     return True
+    '''
+    def find index():
+
+        or ---
+        l = find_mult_index(array, pattern, 1)
+        if len(l) > 0:
+            return l[0]
+        else:
+            return None
+        ---
+
+        loop (i): 0 --> len(text) - len(pattern)
+            #strictly
+            #for/else statement - else only runs if break doesn't happen and full for loop runs
+            loop(j): 0 -> len(pattern) - 1
+                if text[i+j] != pattern[j]
+                    break #ONly out of this inner loop
+                else
+                    return i
+
+    '''
 
 
 def find_index(text, pattern):
@@ -30,24 +51,117 @@ def find_index(text, pattern):
     or None if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
+
+    # if pattern == '':
+    #     return 0
+
+    l = find_all_indexes(text, pattern, 1)
+    print("----")
+    if len(l) > 0:
+        return l[0]
+    else:
+        return None
+
+    # compare_limit = len(text) - len(pattern)
+    # pattern_length = len(pattern) - 1
+    #
+    # for i in range(0, compare_limit):
+    #     for j in range(0, pattern_length):
+    #         if text[i + j] != pattern[j]:
+    #             break
+    #
+    #     else:
+    #         return i
+    # return None
+
+
     # TODO: Implement find_index here (iteratively and/or recursively)
-    if contains(text,pattern) == True:
-        for i in range(0, len(text)-1):
-            print(text[i])
-            if text[i] == pattern[0]:
-                print("Tru")
-                print(text[i:len(pattern)-1])
-                if text[i:len(pattern)] == pattern:
-                    return i
-    return None
+    # if contains(text,pattern) == True:
+    #     for i in range(0, len(text)-1):
+    #         print(text[i])
+    #         if text[i] == pattern[0]:
+    #             print("Tru")
+    #             print(text[i:len(pattern)-1])
+    #             if text[i:len(pattern)] == pattern:
+    #                 return i
+    # return None
 
 
-def find_all_indexes(text, pattern):
+    '''
+    def find_mult_index(array, pattern, flag=0):
+        list = []
+        loop (i): 0 --> len(text) - len(pattern)
+            #strictly
+            #for/else statement - else only runs if break doesn't happen and full for loop runs
+            loop(j): 0 -> len(pattern) - 1
+                if text[i+j] != pattern[j]
+                    break #ONly out of this inner loop
+                else
+                    return list.append(i)
+                    #if using a flag
+                    if flag == 1:
+                        return list
+        return list
+
+    '''
+
+
+def find_all_indexes(text, pattern, flag=0):
     """Return a list of starting indexes of all occurrences of pattern in text,
     or an empty list if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    if len(pattern) <= 0:
+        compare_limit = len(text) - 1
+        pattern_length = len(pattern)
+    else:
+        compare_limit = len(text) - len(pattern)
+        pattern_length = len(pattern) - 1
+
+    list = []
+    print(f"Comparing - {pattern} in {text}")
+    print(f"Compare limit: {compare_limit}")
+
+    for i in range(0, compare_limit+1):
+        print(i)
+        #Check through the entire text to maximum before error
+        print(f"Pattern Length: {pattern_length}")
+        if pattern_length == 0:
+            if pattern == '':
+                list.append(i)
+            elif text[i] != pattern[0]:
+                print("Nope")
+            else:
+                list.append(i)
+
+            if flag == 1:
+                list.append(i)
+                return list
+        else:
+            for j in range(0, pattern_length):
+                print(f"Text: {text[i+j]} - Pattern {pattern[j]}")
+                if text[i+j] != pattern[j]:
+                    print("Break")
+                    break
+            else:
+                if flag == 1:
+                    list.append(i)
+                    return list
+                list.append(i)
+    return list
+
+    # list = []
+    # for i in range(0, compare_limit):
+    #     for j in range(0, pattern_length):
+    #         if text[i + j] != pattern[j]:
+    #             break
+    #
+    #     else:
+    #         if flag == 1:
+    #             return list
+    #         list.append(i)
+    # return list
 
 
 def test_string_algorithms(text, pattern):
@@ -80,7 +194,23 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    print(find_index("happy","ha"))
-    print("--------")
-    print(find_index("pphay","ha"))
+    main()
+
+    # print(find_all_indexes("hahahaha", "ha"))
+    # print("---------")
+    # print(find_all_indexes("abc", "z"))
+    # print(find_all_indexes("abc", "a"))
+    # print("---------")
+
+
+    # print(find_all_indexes("abc", ""))
+    # print("-------")
+    # print(find_index("abc", ""))
+
+
+    # print("---------")
+    # print(find_all_indexes("abcawser", "z"))
+    # print("---------")
+    # print(find_index("happy","ha"))
+    # print("--------")
+    # print(find_index("pphay","ha"))
